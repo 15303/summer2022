bdd615919043b9143f704a4541194e322860242c
qwertyuioplkjhgfd <qwertyuioplkjhgfd@users.noreply.github.com>
2022-06-27 16:26:23 -0700

    robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleTeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Test")
+public class ExampleTeleOp extends LinearOpMode {
+    Robot robot;
+
+    @Override
+    public void runOpMode() {
+        robot = new Robot(this);
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double joystickY = gamepad1.left_stick_y;
+            robot.setPowers(joystickY, joystickY, joystickY, joystickY);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Robot {
+    private OpMode opMode;
+
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+    public Robot(OpMode opMode) {
+        this.opMode = opMode;
+        frontLeft = opMode.hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = opMode.hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = opMode.hardwareMap.get(DcMotor.class, "back_left");
+        backRight = opMode.hardwareMap.get(DcMotor.class, "back_right");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void start() {
+
+    }
+
+    public void setPowers(double fl, double fr, double bl, double br) {
+        frontLeft.setPower(fl);
+        frontRight.setPower(fr);
+        backLeft.setPower(bl);
+        backRight.setPower(br);
+    }
+
+}

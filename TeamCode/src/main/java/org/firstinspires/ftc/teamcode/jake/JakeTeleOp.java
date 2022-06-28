2c752ebdbc8306611388c5c16c0fffaf0bf73cca
jakelovescoding <jakejin2016@gmail.com>
2022-06-27 17:28:15 -0700

    added semicolon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jake/JakeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jake/JakeTeleOp.java
@@ -21,7 +21,7 @@ public class JakeTeleOp extends LinearOpMode {
                 robot.frontLeft.setPower(1);
             }
 
-            robot.setPowers(1,1,1,1)
+            robot.setPowers(1,1,1,1);
             robot.frontLeft.setPower(1);
 
             telemetry.addData("author","Jake");

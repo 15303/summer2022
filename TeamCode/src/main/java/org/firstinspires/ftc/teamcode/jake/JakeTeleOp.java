ab35d27c389b7740f2926cdaf336b43e8d638c44
jakelovescoding <jakejin2016@gmail.com>
2022-06-27 17:26:09 -0700

    Added Telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jake/JakeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jake/JakeTeleOp.java
@@ -21,6 +21,13 @@ public class JakeTeleOp extends LinearOpMode {
                 robot.frontLeft.setPower(1);
             }
 
+            robot.setPowers(1,1,1,1)
+            robot.frontLeft.setPower(1);
+
+            telemetry.addData("author","Jake");
+            telemetry.update();
+
+
         }
     }
 }

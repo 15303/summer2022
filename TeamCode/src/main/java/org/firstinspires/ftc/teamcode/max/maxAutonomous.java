fcd3ee4de6ce75f0c138bbfe7c5a9139867b07f8
Max Sun <maxsun369@gmail.com>
2022-06-29 16:32:05 -0700

    Add Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/maxAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/maxAutonomous.java
@@ -11,22 +11,19 @@ public class maxAutonomous extends LinearOpMode {
     public void runOpMode() {
         Robot robot = new Robot(this);
 
-        robot.lift(0.2);
+        robot.lift(0.1);
         robot.drive(-0.5);
-        sleep(600);
+        sleep(1000);
         robot.turn(0.5);
-        sleep(300);
+        sleep(100);
         robot.spin(0.5);
         sleep(5000);
         robot.turn(-0.5);
-        sleep(300);
+        sleep(100);
         robot.drive(0.8);
         sleep(3000);
         robot.turn(0.5);
-        sleep(500);
+        sleep(200);
         robot.drive(0);
-
-
-
     }
 }

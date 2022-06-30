c13ec509ce869a7a81447da6b168e16888dfee70
qwertyuioplkjhgfd <qwertyuioplkjhgfd@users.noreply.github.com>
2022-06-30 15:17:06 -0700

    fix constant grabbing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/maxTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/max/maxTeleOp.java
@@ -45,6 +45,8 @@ public class maxTeleOp extends LinearOpMode {
             } else {
                 if (isGrabbing) {
                     robot.grab(-0.2);
+                } else {
+                    robot.grab(0);
                 }
             }
 
